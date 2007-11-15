fdea04479ffa9401dc046b955e114e6a64effbd0
1fuji.2taka.3nasbi <1fuji.2taka.3nasbi@0d696c89-863e-0410-890b-6dfc09a99958>
2007-11-15 19:05:44 +0000

    Added a function which devide data matrix into training set and test set

---
+++ src/dataset/devideData.m
@@ -0,0 +1,22 @@
+% function [trainingSet testSet] = devideData(R, sparsity)
+% This function devides the data matrix into trainingSet and testSet
+% Arguments
+%  R - data matrix
+%  sparsity - ratio of the number data in trainigSet and in R
+% Outputs
+%  trainingSet - training set data matrix. Missing elements are filled with
+%  zero.
+%  testSet - test set data. Missing elements are filled with
+%  zero.
+
+function [trainingSet testSet] = devideData(R, sparsity)
+
+trainingSet = zeros(size(R));
+testSet = zeros(size(R));
+rand('twister',sum(100*clock));
+randM = rand(size(R));
+indexTraining = find(randM <= sparsity);
+indexTest = find(randM > sparsity);
+
+trainingSet(indexTraining) = R(indexTraining);
+testSet(indexTest) = R(indexTest);
