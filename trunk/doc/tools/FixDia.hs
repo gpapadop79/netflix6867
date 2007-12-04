79bf0cc7b71cbe8b6fdec04a386545717d7e4422
yaaang.fs <yaaang.fs@0d696c89-863e-0410-890b-6dfc09a99958>
2007-12-04 22:02:02 +0000

    mixture.tex toolchain

--- trunk/doc/tools/FixDia.hs
+++ trunk/doc/tools/FixDia.hs
@@ -1,12 +1,29 @@
 module Main where
 
-import Text.Regex
+import Control.Arrow
+import Data.List
+import Data.String
 import System
+import Text.Regex
 
-subs = lines >>> split "\n" >>> 
+import Debug.Trace
+
+t x = trace (show x) x `seq` x
+
+subs = lines >>> split [""] >>> map t >>> map sub
+  where sub [pat,rep] inp = subRegex (mkRegex pat) inp rep
+
+xform subs = lines >>> map f >>> unlines
+  where f line =
+          let parts = split "\\$" line
+              xformed [a,b,c] = [a, change b, c]
+          in if length parts == 3
+               then join "$" (xformed parts)
+               else line
+        change x  = foldl' apply x subs
+        apply x f = f x
 
 main = do
   args  <- getArgs
-  lines <- readFile $ args !! 1
-  pairs <- (split "\n" >>> )
-  interact $ mkRegex
+  rules <- readFile $ args !! 0
+  interact $ xform $ subs rules
